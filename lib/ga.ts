d450d991c2e3b048be4b5a823eb113f36882ba49
riadbettole <riadbettole@gmail.com>
2023-07-10 14:48:58 +0100

    ga7

--- lib/ga.ts
+++ lib/ga.ts
@@ -1,45 +1,45 @@
-// // declare global {
-// //     interface Window {
-// //       gtag?: any;
-// //     }
-// //   }
+// declare global {
+//     interface Window {
+//       gtag?: any;
+//     }
+//   }
 
-// // export const pageview = (url: any) => {
-// //     if (window !== undefined) {
-// //       window.gtag("config", "G-6D3FB43DHN", {
-// //         page_path: url,
-// //       });
-// //     }
-// //   };
+// export const pageview = (url: any) => {
+//     if (window !== undefined) {
+//       window.gtag("config", "G-6D3FB43DHN", {
+//         page_path: url,
+//       });
+//     }
+//   };
   
-// //   export const event = ({ action, params }:{action: any, params: any}) => {
-// //     if (window !== undefined) {
-// //       window.gtag("event", action, params);
-// //     }
-// //   };
+//   export const event = ({ action, params }:{action: any, params: any}) => {
+//     if (window !== undefined) {
+//       window.gtag("event", action, params);
+//     }
+//   };
 
 
-// export const GA_TRACKING_ID = "G-6D3FB43DHN";
+export const GA_TRACKING_ID = "G-6D3FB43DHN";
 
-// // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-// export const pageview = (url: URL): void => {
-//   window.gtag("config", GA_TRACKING_ID, {
-//     page_path: url,
-//   });
-// };
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: URL): void => {
+  window.gtag("config", GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
 
-// type GTagEvent = {
-//   action: string;
-//   category: string;
-//   label: string;
-//   value: number;
-// };
+type GTagEvent = {
+  action: string;
+  category: string;
+  label: string;
+  value: number;
+};
 
-// // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-// export const event = ({ action, category, label, value }: GTagEvent): void => {
-//   window.gtag("event", action, {
-//     event_category: category,
-//     event_label: label,
-//     value,
-//   });
-// };
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }: GTagEvent): void => {
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value,
+  });
+};
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,71 +1,26 @@
-// // import Document, { Html, Head, Main, NextScript } from "next/document";
-
-// // class MyDocument extends Document {
-// //   render() {
-// //     return (
-// //       <Html>
-// //         <Head>
-// //           <script
-// //             async
-// //             src={`https://www.googletagmanager.com/gtag/js?id=G-6D3FB43DHN`}
-// //           />
-// //           <script
-// //             dangerouslySetInnerHTML={{
-// //               __html: `
-// //     window.dataLayer = window.dataLayer || [];
-// //     function gtag(){dataLayer.push(arguments);}
-// //     gtag('js', new Date());
-// //     gtag('config', 'G-6D3FB43DHN', {
-// //         page_path: window.location.pathname,
-// //     });
-// //     `,
-// //             }}
-// //           />
-// //         </Head>
-// //         <body>
-// //           <Main />
-// //           <NextScript />
-// //         </body>
-// //       </Html>
-// //     );
-// //   }
-// // }
-
-// // export default MyDocument;
-
-
 // import Document, { Html, Head, Main, NextScript } from "next/document";
 
-// import { GA_TRACKING_ID } from "../../lib/ga";
-
-
-// export default class MyDocument extends Document {
-//   render(): JSX.Element {
+// class MyDocument extends Document {
+//   render() {
 //     return (
 //       <Html>
 //         <Head>
-//           {/* enable analytics script only for production */}
-//           {
-//             <>
-//               <script
-//                 async
-//                 src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-//               />
-//               <script
-//                 // eslint-disable-next-line react/no-danger
-//                 dangerouslySetInnerHTML={{
-//                   __html: `
-//             window.dataLayer = window.dataLayer || [];
-//             function gtag(){dataLayer.push(arguments);}
-//             gtag('js', new Date());
-//             gtag('config', '${GA_TRACKING_ID}', {
-//               page_path: window.location.pathname,
-//             });
-//           `,
-//                 }}
-//               />
-//             </>
-//           }
+//           <script
+//             async
+//             src={`https://www.googletagmanager.com/gtag/js?id=G-6D3FB43DHN`}
+//           />
+//           <script
+//             dangerouslySetInnerHTML={{
+//               __html: `
+//     window.dataLayer = window.dataLayer || [];
+//     function gtag(){dataLayer.push(arguments);}
+//     gtag('js', new Date());
+//     gtag('config', 'G-6D3FB43DHN', {
+//         page_path: window.location.pathname,
+//     });
+//     `,
+//             }}
+//           />
 //         </Head>
 //         <body>
 //           <Main />
@@ -75,3 +30,48 @@
 //     );
 //   }
 // }
+
+// export default MyDocument;
+
+
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+import { GA_TRACKING_ID } from "../../lib/ga";
+
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          {/* enable analytics script only for production */}
+          {
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{
+                  __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                }}
+              />
+            </>
+          }
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
